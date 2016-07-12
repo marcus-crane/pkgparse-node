076d3598faa78936b27952d643a474d896d510fa
Marcus Crane <marcus@thingsima.de>
2016-07-13 11:42:29 +1200

    Removed all unneeded slush-pages react files due to a project pivot

--- app.js
+++ app.js
@@ -1,5 +1,14 @@
-import React from 'react'
-import { render } from 'react-dom'
-import Watson from './components/watson'
+var getDependencies = require('./getDependencies')
+var dependencyLookup = require('./dependencyLookup')
 
-render(<Watson />, document.querySelector('main'))
+// Here is where we're going to be able to give a module name and get its dependencies
+var moduleInput = process.argv[2]
+
+// If we don't get a specific module, then run the getDependencies function
+if (!moduleInput) {
+  getDependencies()
+} else {
+  // Else get data about the module then pass it into getDependencies
+  console.log('Fetching data about ' + moduleInput + '...' )
+  dependencyLookup([moduleInput])
+}
