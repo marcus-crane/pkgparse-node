915c51335b7ee4991aaec94fdabc79757673bfef
Marcus Crane <marcus@thingsima.de>
2016-07-13 15:50:24 +1200

    Added flags

--- app.js
+++ app.js
@@ -3,12 +3,23 @@ var getDependencies = require('./lib/getDependencies')
 var dependencyLookup = require('./lib/dependencyLookup')
 
 // Here is where we're going to be able to give a module name and get its dependencies
-var moduleInput = process.argv[2]
+switch(process.argv[2]) {
+  case '-f':
+    var fileLocation = (__dirname + "/package.json")
+    break
+  case '-s':
+    var moduleInput = process.argv[3]
+    break
+
+  default:
+    console.log("Please enter a valid flag.\n\nUse -f to fetch the package.json in your current directory.\n\nUse -s to search for a specific module.")
+    break
+}
 
 // If we don't get a specific module, then run the getDependencies function
-if (!moduleInput) {
-  getDependencies()
-} else {
+if (fileLocation) {
+  getDependencies(fileLocation)
+} else if (moduleInput) {
   // Else get data about the module then pass it into getDependencies
   console.log('\n Fetching data about ' + moduleInput + '...\n' )
   dependencyLookup([moduleInput])
