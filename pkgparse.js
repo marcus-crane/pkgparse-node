fc80c0f82db34ac65a1a38e694e9b3f27228745a
Marcus Crane <marcus@thingsima.de>
2016-07-16 15:18:28 +1200

    Refactored the dependency sorting functionality into its own file and cleaned up the program inputs.

--- pkgparse.js
+++ pkgparse.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-// The above line is what makes it executable
 
 var getDependencies = require('./lib/getDependencies')
 var dependencyLookup = require('./lib/dependencyLookup')
@@ -8,15 +7,34 @@ var chalk = require('chalk')
 
 program
   .version('0.1.0')
-  .usage('[flag] <module name>')
+  .description('pkgparse is a tiny program for finding out info about node modules')
+  .usage('[flag] <options>')
   .option('-f --file', 'scan package.json in current working directory.')
   .option('-s --search [name]', 'search for a specific module')
+  .action(function (flag, name) {
+    flagValue = flag
+    nameValue = name
+  })
   .parse(process.argv)
 
+if (program.file && program.search) {
+  console.log(chalk.yellow("Running file and search together does nothing. Nice try though!"))
+  process.exit(1)
+}
+
 if (program.file) {
-  getDependencies(process.cwd() + "/package.json")
+  getDependencies(process.cwd() + "/test.json")
+}
+
+if (program.file && program.rawArgs[3]) {
+  console.log(chalk.yellow("The -f flag doesn't accept arguments (yet)"))
+  process.exit(1)
 }
 
 if (program.search) {
   dependencyLookup([program.search])
+}
+
+if (program.search && !program.rawArgs[3]) {
+  program.help()
 }
