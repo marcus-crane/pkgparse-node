5f8c44abd839d49d1a7e5ac77ad9e5af741e3e2d
Marcus Crane <marcus@thingsima.de>
2017-03-05 01:31:11 +1300

    Not really sure how to write tests since functions don't return anything, just console log. In the meantime, removed deps

--- test/parseMenu.js
+++
@@ -1,4 +0,0 @@
-module.exports.parseMenu = function(command, args) {
-    console.log('command', command)
-    console.log('args', args)
-}
---
+++ test/search.js
@@ -0,0 +1,4 @@
+const search = require('../lib/search')
+
+describe('search function returns the expected data')
+    it('should return the expected string upon searching')
