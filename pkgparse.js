20c8096f529c0a77c914e3ac076f49cb415e1ae0
Marcus Crane <marcus@thingsima.de>
2017-03-05 01:13:37 +1300

    Now searches both NPM and local packages

--- pkgparse.js
+++ pkgparse.js
@@ -6,31 +6,31 @@ prog
 
     .command('search', 'Get a brief description of what a module does')
     .argument('<module>', 'Module name to look up')
-    .action((args, options, logger) => {
+    .action((args, options) => {
         parseMenu('search', args)
     })
 
-    .command('feast', 'Search run against an entire package.json')
-    //.option('<path>', 'Location of a package.json file to parse')
-    .action((args, options, logger) => {
-        parseMenu('feast', args)
+    .command('feast', 'Search applied to an entire package.json or module')
+    .option('--module <name>', 'Provide a dependency name to see what it\'s made from')
+    .action((args, options) => {
+        parseMenu('feast', options)
     })
 
-    .command('deps', 'Fetch the dependencies that make up a module')
-    .argument('<module>', 'pkgparse will check what dependencies make up this module')
-    .action((args, options, logger) => {
-        parseMenu('deps', args)
-    })
+    // .command('deps', 'Fetch the dependencies that make up a module')
+    // .argument('<module>', 'pkgparse will check what dependencies make up this module')
+    // .action((args, options) => {
+    //     parseMenu('deps', args)
+    // })
 
     .command('open', 'Go straight to the NPM page for a module')
     .argument('<module>', 'The name of the module to navigate to')
-    .action((args, options, logger) => {
+    .action((args, options) => {
         parseMenu('open', args)
     })
 
-    .command('gh', 'Go straight to the Github page for a module if it exists')
+    .command('gh', 'Go straight to the Github page for a module')
     .argument('<module>', 'The name of the module to visit on Github')
-    .action((args, options, logger) => {
+    .action((args, options) => {
         parseMenu('github', args)
     })
 
