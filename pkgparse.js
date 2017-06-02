c0792fa8fdaaeef525fc67880c2c0eee66392a7f
Marcus Crane <marcus@thingsima.de>
2017-06-03 09:02:44 +1200

    Cleaned up, changed helpers to uhh destructing I think is the word and changed to 2 spaces

--- pkgparse.js
+++ pkgparse.js
@@ -4,30 +4,30 @@ const prog = require('caporal')
 const parseMenu = require('./lib/parseMenu')
 
 prog
-    .version('2.0.0')
+  .version('2.0.0')
 
-    .command('search', 'Get a brief description of what a module does')
-    .argument('<module>', 'Module name to look up')
-    .action((args, options) => {
-        parseMenu('search', args)
-    })
+  .command('search', 'Get a brief description of what a module does')
+  .argument('<module>', 'Module name to look up')
+  .action((args, options) => {
+    parseMenu('search', args)
+  })
 
-    .command('feast', 'Search applied to an entire package.json or module')
-    .option('--module <name>', 'Provide a dependency name to see what it\'s made from')
-    .action((args, options) => {
-        parseMenu('feast', options)
-    })
+  .command('feast', 'Search applied to an entire package.json or module')
+  .option('--module <name>', 'Provide a dependency name to see what it\'s made from')
+  .action((args, options) => {
+    parseMenu('feast', options)
+  })
 
-    .command('open', 'Go straight to the NPM page for a module')
-    .argument('<module>', 'The name of the module to navigate to')
-    .action((args, options) => {
-        parseMenu('open', args)
-    })
+  .command('open', 'Go straight to the NPM page for a module')
+  .argument('<module>', 'The name of the module to navigate to')
+  .action((args, options) => {
+    parseMenu('open', args)
+  })
 
-    .command('gh', 'Go straight to the Github page for a module')
-    .argument('<module>', 'The name of the module to visit on Github')
-    .action((args, options) => {
-        parseMenu('github', args)
-    })
+  .command('gh', 'Go straight to the Github page for a module')
+  .argument('<module>', 'The name of the module to visit on Github')
+  .action((args, options) => {
+    parseMenu('github', args)
+  })
 
 prog.parse(process.argv)
