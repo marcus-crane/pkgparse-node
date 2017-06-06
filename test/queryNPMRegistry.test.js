e7712f60c979e9c8fcf7bc7d000dde52c0647c78
Marcus Crane <marcus@thingsima.de>
2017-06-06 20:21:39 +1200

    Added request, queryNPM and test for queryNPM

---
+++ test/queryNPMRegistry.test.js
@@ -0,0 +1,16 @@
+const assert = require('assert')
+const queryNPMRegistry = require('../lib/queryNPMRegistry')
+
+describe('queryNPMRegistry', function () {
+  it('should return a valid object', async function () {
+    this.timeout(5000)
+    const query = await queryNPMRegistry('mocha')
+    assert.equal(query.name, 'mocha')
+  })
+
+  it('should return 404 if the package does not exist', async function () {
+    this.timeout(5000)
+    const query = await queryNPMRegistry('hdriuhg')
+    assert.equal(query, 404)
+  })
+})
