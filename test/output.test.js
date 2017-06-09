1b243f90be493308586e91796f98534d57c3c0f1
Marcus Crane <marcus@thingsima.de>
2017-06-09 21:43:30 +1200

    Wrote a test for log404

--- test/output.test.js
+++ test/output.test.js
@@ -1,6 +1,6 @@
 const { stdout } = require('test-console')
 const { deepStrictEqual } = require('assert')
-const { colour, logDescription } = require('../lib/output')
+const { colour, logDescription, log404, logThanks, logTypes } = require('../lib/output')
 
 describe('output', function () {
   describe('logDescription', function () {
@@ -18,6 +18,15 @@ describe('output', function () {
       logDescription('FAKEPACKAGE', description)
       inspect.restore()
       deepStrictEqual(`${colour.red}?? fakepackage =>${colour.white} ${description}\n`, inspect.output[0])
+    })
+  })
+
+  describe('log404', function () {
+    it('should output with red text when given a module name', function () {
+      const inspect = stdout.inspect()
+      log404('TwitchForDogs')
+      inspect.restore()
+      deepStrictEqual(`${colour.red}✘  TwitchForDogs${colour.white} does not exist?!\n`, inspect.output[0])
     })
   })
 })
