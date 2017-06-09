166954fd5286018bf052a45ae614726821b45535
Marcus Crane <marcus@thingsima.de>
2017-06-09 21:11:26 +1200

    Fed the linter

--- test/feast.test.js
+++ test/feast.test.js
@@ -6,17 +6,15 @@ const feast = require('../lib/feast')
 describe('feast', function () {
   it('should output the dependencies of a module along with their descriptions', async function () {
     const inspect = stdout.inspect()
-    const moduleName = 'uuid'
-    await feast(moduleName)
+    await feast('uuid')
     inspect.restore()
     deepStrictEqual(`${colour.green}‣  mocha =>${colour.white} simple, flexible, fun test framework\n`, inspect.output[0])
   })
 
   it('should output an error message if the module does not exist', async function () {
     const inspect = stdout.inspect()
-    const moduleName = 'frumpywatermelon'
-    await feast(moduleName)
+    await feast('frumpywatermelon')
     inspect.restore()
-    deepStrictEqual(`${colour.red}✘  ${moduleName}${colour.white} does not exist?!\n`, inspect.output[0])
+    deepStrictEqual(`${colour.red}✘  frumpywatermelon${colour.white} does not exist?!\n`, inspect.output[0])
   })
 })
